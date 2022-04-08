9482e6e91c45f78325267412343746491976a28a
Metin Arslan <arsmetin90@gmail.com>
2022-04-08 16:24:27 +0300

    Week 7

--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.DataSuccessResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAcces.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
 
@@ -22,9 +26,17 @@ public class ProductManager implements ProductService{
 	}
 
 	@Override
-	public List<Product> getAll() {
+	public DataResult<List<Product>> getAll() {
 
-		return this.productDao.findAll();
+		return new DataSuccessResult<List<Product>>(this.productDao.findAll(),"Data Listelendi");
+		
+	}
+
+	@Override
+	public Result add(Product product) {
+		// TODO Auto-generated method stub
+		this.productDao.save(product);
+		return new SuccessResult("Ürün Eklendi");
 	}
 
 }
